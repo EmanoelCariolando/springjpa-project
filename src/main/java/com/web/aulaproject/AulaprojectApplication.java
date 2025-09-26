f84bb6d548d01e183fad1844cc33c449b9515317
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-09-26 10:46:03 -0300

    First Commit

---
+++ src/main/java/com/web/aulaproject/AulaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.web.aulaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/aulaproject/AulaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.aulaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
