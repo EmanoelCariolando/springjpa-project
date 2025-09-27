e693e340c098fe9874e697f2fcb7ce02a1fb7215
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-09-27 09:30:27 -0300

    Spring Project - Update
    Introduced the User entity class with basic fields and methods. Added UserResource REST controller with a GET endpoint that returns a sample User. Minor formatting changes in AulaprojectApplication.

--- src/main/java/com/web/aulaproject/AulaprojectApplication.java
+++ src/main/java/com/web/aulaproject/AulaprojectApplication.java
@@ -1,10 +1,12 @@
 package com.web.aulaproject;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AulaprojectApplication {
+public class AulaprojectApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaprojectApplication.class, args);
